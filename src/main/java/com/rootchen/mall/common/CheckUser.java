1e87b717b2ab9137628e23706926f5ccdbba217d
tangczc <Mytczc521@outlook.com>
2019-05-27 15:15:46 +0800

    测试自动部署

--- src/main/java/com/rootchen/mall/common/CheckUser.java
+++ src/main/java/com/rootchen/mall/common/CheckUser.java
@@ -13,14 +13,13 @@ import javax.servlet.http.HttpSession;
 public class CheckUser {
     /**
      * 用户是否登录
-     *
      * @param session
      * @return Boolean
      */
-    public static Boolean isLoginSuccess(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
+    public static Boolean isLoginSuccess(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
         System.out.println(new Gson().toJson(user));
-        if (user != null) {
+        if(user != null){
             return true;
         }
         return false;
@@ -28,13 +27,12 @@ public class CheckUser {
 
     /**
      * 用户是否是管理员
-     *
      * @param session
      * @return Boolean
      */
-    public static Boolean isAdmin(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user.getRole() == Const.Role.ROLE_ADMIN) {
+    public static Boolean isAdmin(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user.getRole() == Const.Role.ROLE_ADMIN){
             return true;
         }
         return false;
