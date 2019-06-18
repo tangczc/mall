8b22adb354426aea638a55c6a40e7b12b6b966e7
tangczc <Mytczc521@outlook.com>
2019-06-18 16:50:38 +0800

    购物车模块

--- src/main/java/com/rootchen/mall/mapper/CartMapper.java
+++ src/main/java/com/rootchen/mall/mapper/CartMapper.java
@@ -1,8 +1,10 @@
 package com.rootchen.mall.mapper;
 
-import com.rootchen.mall.entity.Cart;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rootchen.mall.entity.Cart;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -20,5 +22,50 @@ public interface CartMapper extends BaseMapper<Cart> {
      * @param productId
      * @return
      */
-    Cart selectByUserIdAndProduct(@Param("userId") Long userId,@Param("productId") Long productId);
+    Cart selectByUserIdAndProduct(@Param("userId") Long userId, @Param("productId") Long productId);
+
+    /**
+     * 查询用户的购物车未选商品数量
+     *
+     * @param userId
+     * @return
+     */
+    int selectCartProductCheckedStatusByUserId(@Param("userId") Long userId);
+
+    /**
+     * 查询用户购物车列表
+     *
+     * @param userId
+     * @return
+     */
+    List<Cart> selectCartByUserId(@Param("userId") Long userId);
+
+    /**
+     * 删除购物车商品
+     *
+     * @param userId
+     * @param productIdList
+     * @return
+     */
+    int deleteByUserIdProductIds(@Param("userId") Long userId, @Param("productIdList") List<String> productIdList);
+
+    /**
+     * 全选 反全选
+     *
+     * @param userId
+     * @param productId
+     * @param checked
+     * @return
+     */
+    int checkOrUncheckedProduct(@Param("userId") Long userId, @Param("productId") Long productId, @Param("checked") Integer checked);
+
+    /**
+     * 获取购物车商品总数
+     *
+     * @param userId
+     * @return
+     */
+    int selectCartProductCount(@Param("userId") Long userId);
+
+
 }
