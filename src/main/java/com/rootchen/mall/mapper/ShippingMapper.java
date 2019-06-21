6dd4d6dfe69209375f4973ccaaf41f5236b39b8c
Lichen <My_521_tczc@icould.com>
2019-06-21 08:05:09 +0800

    收获地址模块

--- src/main/java/com/rootchen/mall/mapper/ShippingMapper.java
+++ src/main/java/com/rootchen/mall/mapper/ShippingMapper.java
@@ -1,7 +1,10 @@
 package com.rootchen.mall.mapper;
 
-import com.rootchen.mall.entity.Shipping;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rootchen.mall.entity.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,5 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface ShippingMapper extends BaseMapper<Shipping> {
 
+    List<Shipping> selectShippingList(@Param("userId") Long userId);
 }
